685beecc624e64c538aa2597d278c951f51f8af4
Moiz Ali <moizali2577@gmail.com>
2025-12-02 19:37:45 +0500

    Configure: Update organizationName for GitHub Pages deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -22,7 +22,7 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'PanaverseDAO', // Usually your GitHub org/user name.
+  organizationName: 'hina-ali651', // Usually your GitHub org/user name.
   projectName: 'ai-robotics-textbook', // Usually your repo name.
 
   onBrokenLinks: 'throw',
